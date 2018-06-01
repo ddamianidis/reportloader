3afa3dd774679b11e61901f284e4729ce99abd79
damianos <damianosd@tailwindemea.net>
2018-06-01 15:03:16 +0300

    change expected data for push test

--- reportloader/tests/data_pusher.py
+++ reportloader/tests/data_pusher.py
@@ -58,20 +58,25 @@ class DataPusherTestCase(unittest.TestCase):
     #@unittest.skip('just skip')       
     def test_1_push_all(self):
         self.maxDiff = None
-        expected = {'rejected_size': 2, 'rejected_pub_fee': 0, 'rejected_id': 8, 
-                    'inserted': 8267, 'inserted_eurohoops': 528, 'found': 8285, 
-                    'rejected_revenue': 2, 'rejected_inventory': 6, 
-                    'messages': ['Platform adsense. Rejecting "Houselife Adsense 300x250". Could_not_parse_placement_id', 
-                                 'Platform adsense. Rejecting "YL_300x250". Could_not_parse_placement_id', 
-                                 'Platform adsense. Rejecting "TheFrog_300x250". Could_not_parse_placement_id', 
-                                 'Platform adsense. Rejecting "Test_MSN_Remnant_728x90". Could_not_parse_placement_id', 
-                                 'Platform adsense. Rejecting "Test_MSN_Remnant_300x250". Could_not_parse_placement_id', 
-                                 'Platform adx. Rejecting "(No tag)". Could_not_parse_placement_id', 
-                                 'Platform adx. Rejecting "11974846_apkroids.com_ros300x250". Could_not_parse_placement_size', 
-                                 'Platform teads. Rejecting "77422 - inRead -  sportfm.gr5687603". Could_not_parse_placement_id', 
-                                 'Platform criteo. Rejecting "12001599_gazzetta.gr_adblock-3_300x250". revenue_is_not_float', 
-                                 'Platform criteo. Rejecting "11777559_ma7room.com_ros_970x250". revenue_is_not_float', 
-                                 'Platform criteo. Rejecting "12341055_missbloom.bg_homepage-ros-perf_970x250". Could_not_find_it_in_inventory_table', 'Platform criteo. Rejecting "12341048_missbloom.bg_homepage-ros-b-perf_300x250". Could_not_find_it_in_inventory_table', 'Platform criteo. Rejecting "12341051_missbloom.bg_homepage-ros-b-perf_300x600". Could_not_find_it_in_inventory_table', 'Platform taboola. Rejecting "insider#300250#GR-r11409755". Could_not_parse_placement_size', 'Platform taboola. Rejecting "mother#GR-p9964478_not_used - DEACTIVATED". Could_not_parse_placement_id', 'Platform rubicon. Rejecting "12341050_missbloom.bg_homepage-ros-a-perf_300x600". Could_not_find_it_in_inventory_table', 'Platform rubicon. Rejecting "12341055_missbloom.bg_homepage-ros-perf_970x250". Could_not_find_it_in_inventory_table', 'Platform rubicon. Rejecting "12341056_missbloom.bg_homepage-ros-perf_728x90". Could_not_find_it_in_inventory_table']}
+        expected = {'found': 8285, 'inserted': 8267, 'inserted_eurohoops': 528, 'rejected_id': 8, 'rejected_size': 2, 'rejected_inventory': 6, 'rejected_pub_fee': 0, 'rejected_revenue': 2, 'messages': 
+                    ['Platform adsense. Rejecting "TheFrog_300x250". Could_not_parse_placement_id', 
+                    'Platform adsense. Rejecting "Test_MSN_Remnant_300x250". Could_not_parse_placement_id', 
+                    'Platform adsense. Rejecting "Test_MSN_Remnant_728x90". Could_not_parse_placement_id', 
+                    'Platform adsense. Rejecting "Houselife Adsense 300x250". Could_not_parse_placement_id',
+                    'Platform adsense. Rejecting "YL_300x250". Could_not_parse_placement_id', 
+                    'Platform adx. Rejecting "11974846_apkroids.com_ros300x250". Could_not_parse_placement_size', 
+                    'Platform adx. Rejecting "(No tag)". Could_not_parse_placement_id', 
+                    'Platform teads. Rejecting "77422 - inRead -  sportfm.gr5687603". Could_not_parse_placement_id', 
+                    'Platform criteo. Rejecting "12001599_gazzetta.gr_adblock-3_300x250". revenue_is_not_float', 
+                    'Platform criteo. Rejecting "12341051_missbloom.bg_homepage-ros-b-perf_300x600". Could_not_find_it_in_inventory_table', 
+                    'Platform criteo. Rejecting "12341048_missbloom.bg_homepage-ros-b-perf_300x250". Could_not_find_it_in_inventory_table', 
+                    'Platform criteo. Rejecting "11777559_ma7room.com_ros_970x250". revenue_is_not_float', 
+                    'Platform criteo. Rejecting "12341055_missbloom.bg_homepage-ros-perf_970x250". Could_not_find_it_in_inventory_table', 
+                    'Platform taboola. Rejecting "mother#GR-p9964478_not_used - DEACTIVATED". Could_not_parse_placement_id', 
+                    'Platform taboola. Rejecting "insider#300250#GR-r11409755". Could_not_parse_placement_size', 
+                    'Platform rubicon. Rejecting "12341050_missbloom.bg_homepage-ros-a-perf_300x600". Could_not_find_it_in_inventory_table', 
+                    'Platform rubicon. Rejecting "12341056_missbloom.bg_homepage-ros-perf_728x90". Could_not_find_it_in_inventory_table', 
+                    'Platform rubicon. Rejecting "12341055_missbloom.bg_homepage-ros-perf_970x250". Could_not_find_it_in_inventory_table']}
         
         self.updateReports()
         
@@ -79,6 +84,13 @@ class DataPusherTestCase(unittest.TestCase):
         ret_data = JsonRPCClient("tcp://localhost:5552").rpc_call('push_data', platform='all',
                                             date = '2018-01-20', mode='test')
         print(ret_data['result'])
+        
+         # order messages in both dicts
+        if 'messages' in expected:
+             expected['messages'] = sorted(expected['messages'])
+        
+        if 'messages' in ret_data['result']:
+             ret_data['result']['messages'] = sorted(ret_data['result']['messages'])
         
         self.assertDictEqual(ret_data['result'], expected,  'Push to UI is correct')
         #self.assertNotEqual(ret_data['result'], False,  'Push to UI is correct')
